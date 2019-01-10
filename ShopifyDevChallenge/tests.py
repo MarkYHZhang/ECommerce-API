3ec7d38995e379717b9a1400f2b4736f24cb3738
Mark Zhang <markzhang.inbox@gmail.com>
2019-01-09 23:57:12 -0500

    Adding unit testing

---
+++ ShopifyDevChallenge/tests.py
@@ -0,0 +1,22 @@
+from django.test import TestCase
+from .models import Product
+from .models import Cart
+
+
+class ProductTest(TestCase):
+
+    @staticmethod
+    def create_product(id="test_id", name="Test Product Name", price=100.0,count=10):
+        return Product.objects.create(id,name,price,count)
+
+    def test_product_creation(self):
+        id = "sample_id"
+        name = "Nice Product Name"
+        price = 0.99
+        count = 1000
+        product = self.create_product(id, name, price, count)
+        self.assertTrue(isinstance(product, Product))
+        self.assertEqual(product.id,id)
+        self.assertEqual(product.title,name)
+        self.assertEqual(product.price,price)
+        self.assertEqual(product.inventory_count,count)
