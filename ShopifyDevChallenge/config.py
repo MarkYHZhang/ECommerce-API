2f319d7de397d4846f9b0357045452c949bd9dfa
Mark Zhang <markzhang.inbox@gmail.com>
2019-01-11 17:11:38 -0500

    Completed DB unit testing

--- ShopifyDevChallenge/config.py
+++ ShopifyDevChallenge/config.py
@@ -2,6 +2,3 @@ DB_NAME = 'shopifydev'
 DB_USER = 'mark@myhzsql'
 DB_PASSWORD = 'Youmadbro2013?'
 DB_HOST = 'myhzsql.postgres.database.azure.com'
-
-# FACEBOOK_KEY = '510375389452543'
-# FACEBOOK_SECRET = '991546bcfd09130474e2b6a2a90906c5'
--- ShopifyDevChallenge/tests.py
+++ ShopifyDevChallenge/tests.py
@@ -9,6 +9,14 @@ class ProductTest(TestCase):
     def create_product(id="test_id", name="Test Product Name", price=100.0,count=10):
         return Product.objects.create(id=id,title=name,price=price,inventory_count=count)
 
+    @staticmethod
+    def create_cart(id="test_id", items=None, item_quantities=None, cost = 0):
+        if item_quantities is None:
+            item_quantities = []
+        if items is None:
+            items = []
+        return Cart.objects.create(id=id,items=items,item_quantities=item_quantities, cost=cost)
+
     def test_product_creation(self):
         id = "sample_id"
         name = "Nice Product Name"
@@ -20,3 +28,15 @@ class ProductTest(TestCase):
         self.assertEqual(product.title,name)
         self.assertEqual(product.price,price)
         self.assertEqual(product.inventory_count,count)
+
+    def test_cart_creation(self):
+        id="test_Cart_id"
+        items = ["product1","product2"]
+        item_quantities = [10,20]
+        cost=100
+        cart = self.create_cart(id,items,item_quantities,cost)
+        self.assertTrue(isinstance(cart, Cart))
+        self.assertEqual(id,cart.id)
+        self.assertEqual(items,cart.item)
+        self.assertEqual(item_quantities,cart.item_quantities)
+        self.assertEqual(cost,cart.cost)
